ac0eba3ee321219fc2406b16bdc0dafae2a7d7ba
krzosdominik <krzosdominik@gmail.com>
2019-11-28 10:02:51 +0100

    Updates

---
+++ precache-manifest.c13352be2278d9aaa2f336273bf4e23d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc8d20012dc99c50d12a315eec0a245",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "11c0dae52181ff468546",
+    "url": "/react-weather/static/css/2.669337a1.chunk.css"
+  },
+  {
+    "revision": "11c0dae52181ff468546",
+    "url": "/react-weather/static/js/2.3da5f870.chunk.js"
+  },
+  {
+    "revision": "f55e4ee6baaa6420e42c",
+    "url": "/react-weather/static/js/main.f7566a6f.chunk.js"
+  },
+  {
+    "revision": "96c4b179dfdf7b944eb5",
+    "url": "/react-weather/static/js/runtime-main.74656030.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-weather/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-weather/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-weather/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-weather/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-weather/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
