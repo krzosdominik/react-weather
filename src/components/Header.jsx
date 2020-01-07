6f83b8c0a951ccd62af3838e65f50cc9f7ccdf90
krzosdominik <krzosdominik@gmail.com>
2020-01-07 22:45:35 +0100

    change html5 anchor to react Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className='mb-3'>
+    <header className="mb-3">
       <nav className="navbar navbar-dark bg-secondary d-flex justify-content-center">
-        <a className="navbar-brand pl-4" style={{fontFamily: 'Lobster', fontSize: '1.5rem'}} href="/">
-          <img src="http://openweathermap.org/img/wn/11n.png" alt='logo' />
-          Pogodynka
-        </a>
+        <Link
+          to="/"
+          className="navbar-brand pl-4"
+          style={{ fontFamily: 'Lobster', fontSize: '1.5rem' }}
+        >
+          <img src="http://openweathermap.org/img/wn/11n.png" alt="logo" />
+          <span>Pogodynka</span>
+        </Link>
       </nav>
     </header>
   );
